9400011fd7e791ae899401b94eca30a1d995a026
White Russian <whiterussian@Whites-iMac.Dlink>
2018-08-25 13:06:14 +0700

    init commit

---
+++ src/main/java/com/github/vvhiterussian/restmate/RestMateApplication.java
@@ -0,0 +1,12 @@
+package com.github.vvhiterussian.restmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMateApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/vvhiterussian/restmate/RestMateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.vvhiterussian.restmate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestMateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
