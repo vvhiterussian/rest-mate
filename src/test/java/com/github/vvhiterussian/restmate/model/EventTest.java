446427bcc5c6fd6367f1b22ea941a37c75d0d65c
vvhiterussian <rokononov@gmail.com>
2018-08-27 01:58:34 +0700

    User and Event tests verifying calls of the mock DAO objects

---
+++ src/test/java/com/github/vvhiterussian/restmate/model/EventTest.java
@@ -0,0 +1,61 @@
+package com.github.vvhiterussian.restmate.model;
+
+import com.github.vvhiterussian.restmate.dao.MatesDAO;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
+public class EventTest {
+
+    @Test
+    public void TestAddMateToEvent() {
+        MatesDAO matesDAO = mock(MatesDAO.class);
+        User organizer = mock(User.class);
+        Event event = new Event("Test event", "Test event", new EventType("Test event type", EventKind.FUN), organizer, matesDAO);
+
+        User mate = mock(User.class);
+        try {
+            event.addMate(mate);
+        } catch (RuntimeException e) { }
+
+        verify(matesDAO, times(1)).addMateToEvent(event, mate);
+        verify(matesDAO, never()).removeMateFromEvent(any(Event.class), any(User.class));
+        verify(matesDAO, never()).getMatesByEvent(any(Event.class));
+    }
+
+    @Test
+    public void TestRemoveMateFromEvent() {
+        MatesDAO matesDAO = mock(MatesDAO.class);
+        User organizer = mock(User.class);
+        Event event = new Event("Test event", "Test event", new EventType("Test event type", EventKind.FUN), organizer, matesDAO);
+
+        User mate = mock(User.class);
+        try {
+            event.removeMate(mate);
+        } catch (RuntimeException e) { }
+
+        verify(matesDAO, times(1)).removeMateFromEvent(event, mate);
+        verify(matesDAO, never()).addMateToEvent(any(Event.class), any(User.class));
+        verify(matesDAO, never()).getMatesByEvent(any(Event.class));
+    }
+
+    @Test
+    public void TestGetMatesByEvent() {
+        MatesDAO matesDAO = mock(MatesDAO.class);
+        User organizer = mock(User.class);
+        Event event = new Event("Test event", "Test event", new EventType("Test event type", EventKind.FUN), organizer, matesDAO);
+
+        User user = mock(User.class);
+        List<User> userList = Arrays.asList(user);
+        when(matesDAO.getMatesByEvent(event)).thenReturn(userList);
+
+        Assert.assertEquals(userList, event.getMates());
+        verify(matesDAO, never()).removeMateFromEvent(any(Event.class), any(User.class));
+        verify(matesDAO, never()).addMateToEvent(any(Event.class), any(User.class));
+    }
+}
---
+++ src/test/java/com/github/vvhiterussian/restmate/model/UserTest.java
@@ -0,0 +1,101 @@
+package com.github.vvhiterussian.restmate.model;
+
+import com.github.vvhiterussian.restmate.dao.EventsDAO;
+import com.github.vvhiterussian.restmate.dao.MatesDAO;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class UserTest {
+
+    static EventsDAO emptyEventsDAO = new EventsDAO() {
+        @Override
+        public List<Event> findEvents(EventKind eventKind, EventType eventType, String name) {
+            return null;
+        }
+
+        @Override
+        public void addEvent(Event event) {
+
+        }
+    };
+
+    static MatesDAO emptyMatesDAO = new MatesDAO() {
+        @Override
+        public List<User> getMatesByEvent(Event event) {
+            return null;
+        }
+
+        @Override
+        public void addMateToEvent(Event event, User user) {
+
+        }
+
+        @Override
+        public void removeMateFromEvent(Event event, User user) {
+
+        }
+    };
+
+    @Test
+    public void TestUserNotOrganizerCannotAddEventWithEmptyDAOs() {
+        AtomicInteger eventCnt = new AtomicInteger(0);
+
+        EventsDAO eventsDAO = new EventsDAO() {
+            @Override
+            public List<Event> findEvents(EventKind eventKind, EventType eventType, String name) {
+                return null;
+            }
+
+            @Override
+            public void addEvent(Event event) {
+                eventCnt.incrementAndGet();
+            }
+        };
+
+        boolean isOrganizer = false;
+        User user = new User("test-user-1", "qweqwe", isOrganizer, eventsDAO);
+        Event event = new Event("Test event", "Test event", new EventType("Test event type", EventKind.FUN), user, emptyMatesDAO);
+        try {
+            user.addNewEvent(event);
+        } catch (RuntimeException e) {}
+
+        assertEquals(0, eventCnt.get());
+    }
+
+
+    //Tests with Mockito
+    @Test
+    public void TestUserNotOrganizerCannotAddEvent() {
+        EventsDAO eventsDAO = mock(EventsDAO.class);
+        Event event = mock(Event.class);
+
+        boolean isOrganizer = false;
+        User user = new User("test-user-1", "qweqwe", isOrganizer, eventsDAO);
+        try {
+            user.addNewEvent(event);
+        } catch (RuntimeException e) {}
+
+        verify(eventsDAO, never()).addEvent(event);
+        verify(eventsDAO, never()).findEvents(any(EventKind.class), any(EventType.class), anyString());
+    }
+
+    @Test
+    public void TestUserIsOrganizerCanAddEvent() {
+        EventsDAO eventsDAO = mock(EventsDAO.class);
+        Event event = mock(Event.class);
+
+        boolean isOrganizer = true;
+        User user = new User("test-user-1", "qweqwe", isOrganizer, eventsDAO);
+        try {
+            user.addNewEvent(event);
+        } catch (RuntimeException e) {}
+
+        verify(eventsDAO, times(1)).addEvent(event);
+        verify(eventsDAO, never()).findEvents(any(EventKind.class), any(EventType.class), anyString());
+    }
+}
