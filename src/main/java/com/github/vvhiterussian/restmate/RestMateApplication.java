9dd8c35e7e0a75382040ba27a4e68e6c2cfc0b03
vvhiterussian <rokononov@gmail.com>
2018-09-01 19:35:13 +0700

    servlet added

--- src/main/java/com/github/vvhiterussian/restmate/RestMateApplication.java
+++ src/main/java/com/github/vvhiterussian/restmate/RestMateApplication.java
@@ -1,12 +1,10 @@
 package com.github.vvhiterussian.restmate;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class RestMateApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(RestMateApplication.class, args);
-    }
-}
+//
+//@SpringBootApplication
+//public class RestMateApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(RestMateApplication.class, args);
+//    }
+//}
---
+++ src/main/java/com/github/vvhiterussian/restmate/web/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.github.vvhiterussian.restmate.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/users/*")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("Hello, World!");
+    }
+}
